0cf8ed8a46c6775e9ae3b8c4911c718d9adef5bf
Alberto <aiglesias4@my.bcit.ca>
2020-04-17 15:34:16 -0700

    fixes to adding movies

--- app/src/main/java/com/example/final_exam/AddMovieActivity.java
+++ app/src/main/java/com/example/final_exam/AddMovieActivity.java
@@ -3,6 +3,7 @@ package com.example.final_exam;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,9 +23,12 @@ public class AddMovieActivity extends AppCompatActivity {
     }
 
     public void onClickAddButton(View view) {
-        String movieTitle = findViewById(R.id.movieTitle).toString();
-        String movieDesc = findViewById(R.id.movieDescription).toString();
-        String movieImdb = findViewById(R.id.movieImdbLink).toString();
+        EditText movieTitleHolder = findViewById(R.id.movieTitle);
+        String movieTitle = movieTitleHolder.getText().toString();
+        EditText movieDescHolder = findViewById(R.id.movieDescription);
+        String movieDesc = movieDescHolder.getText().toString();
+        EditText movieImdbHolder = findViewById(R.id.movieImdbLink);
+        String movieImdb = movieImdbHolder.getText().toString();
         Movie newMovie = new Movie(movieTitle, movieDesc, movieImdb);
 
         DatabaseReference newMovieLocation = database.push();
