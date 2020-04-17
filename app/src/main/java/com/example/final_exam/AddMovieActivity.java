9436ce2bab6b9a1e3f55cd757f5e37a4aa65cd8f
Alberto <aiglesias4@my.bcit.ca>
2020-04-17 14:47:50 -0700

    added extra activities

---
+++ app/src/main/java/com/example/final_exam/AddMovieActivity.java
@@ -0,0 +1,15 @@
+package com.example.final_exam;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
